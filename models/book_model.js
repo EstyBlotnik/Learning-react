2987d13493eeb8ccfd6815c8359e12a553cbf441
Esty <estyblotnik@gmail.com>
2023-10-28 18:54:56 +0300

    commit

---
+++ models/book_model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    publishing_year:{
+        type: Number,
+        default: 2023,
+    },
+    Published_by: {
+        type: String,
+        required: true,
+    },
+    Summary: {
+        type: String,
+        required: true,
+    },
+    state_of_book: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('Book',bookSchema)
